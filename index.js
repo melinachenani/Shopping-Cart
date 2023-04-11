1a7ff20190af74163db7435c1e7147a67d26cd0a
Melina Chenani <122263126+melinachenani@users.noreply.github.com>
2023-04-11 19:08:13 +0330

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ toggler.addEventListener("click", (e) => {
     navbar.classList.toggle("nav__expanded")
 });
 
+//Selectors
+const cartModal = document.querySelector(".cart");
+const openModal = document.querySelector(".btn-shopping-cart");
+const closeModal = document.querySelector(".cart-item-confirm");
+const backdrop = document.querySelector(".backdrop");
+
+
+//Event Listeners
+openModal.addEventListener("click", showModalFunction);
+closeModal.addEventListener("click", closeModalFunction);
+backdrop.addEventListener("click", closeModalFunction);
+
+//Functions
+function showModalFunction (){
+    backdrop.style.display= "block";
+    cartModal.style.opacity= "1";
+}
+function closeModalFunction (){
+    cartModal.style.opacity= "0";
+    backdrop.style.display= "none";
+}
