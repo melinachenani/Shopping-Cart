694f4412b1cbddb0118a3e8fc3772222bb6b2dfe
Melina Chenani <122263126+melinachenani@users.noreply.github.com>
2023-04-15 18:52:03 +0330

    Create products.js

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+    {
+        id: 1,
+        title: "Queen's Bed",
+        price:  1250,
+        imageUrl: "./assets/Pictures/product-1.jpeg",
+    },
+    {
+        id: 2,
+        title: "Modern Bed",
+        price: 2500,
+        imageUrl: "./assets/Pictures/product-2.jpeg",
+    },
+    {
+        id: 3,
+        title: "California King Bed",
+        price: 3000,
+        imageUrl: "./assets/Pictures/product-3.jpeg",
+    },
+    {
+        id: 4,
+        title: "Full Size Bed",
+        price: 800,
+        imageUrl: "./assets/Pictures/product-4.jpeg",
+    },
+    {
+        id: 5,
+        title: "Samsung Smart Refrigerator",
+        price: 8000,
+        imageUrl: "./assets/Pictures/product-5.jpeg",
+    },
+    {
+        id: 6,
+        title: "Wavy Sofa Chair",
+        price: 600,
+        imageUrl: "./assets/Pictures/product-6.jpeg",
+    },
+    {
+        id: 7,
+        title: "L Couch",
+        price: 980,
+        imageUrl: "./assets/Pictures/product-7.jpeg",
+    },
+    {
+        id: 8,
+        title: "Danish Chair",
+        price: 168,
+        imageUrl: "./assets/Pictures/product-8.jpeg",
+    },
+];
